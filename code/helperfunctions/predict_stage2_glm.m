e756e8866e09a9f0b249e5f1a30f903dd097d3bb
Lioba Berndt <lioba.berndt@gmail.com>
2026-04-21 11:33:35 +0100

    merged negative binomila directly in function call

--- code/helperfunctions/predict_stage2_glm.m
+++ code/helperfunctions/predict_stage2_glm.m
@@ -1,5 +1,6 @@
 function yhat = predict_stage2_glm(mdl, X)
     if isstruct(mdl) && isfield(mdl, 'model_class') && strcmp(mdl.model_class, 'custom_negbin_glm')
+        % Custom NB model uses explicit intercept + linear predictor.
         if isempty(X)
             Z = ones(1, numel(mdl.beta));
         else
--- code/helperfunctions/stage2_glm_coefficients.m
+++ code/helperfunctions/stage2_glm_coefficients.m
@@ -1,7 +1,4 @@
 function coefT = stage2_glm_coefficients(mdl)
-    if isstruct(mdl) && isfield(mdl, 'model_class') && strcmp(mdl.model_class, 'custom_negbin_glm')
-        coefT = mdl.Coefficients;
-    else
-        coefT = mdl.Coefficients;
-    end
+    % Shared accessor keeps downstream code agnostic to model class.
+    coefT = mdl.Coefficients;
 end
